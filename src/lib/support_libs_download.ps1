6b8218ee064cbc9b375e507caf3cd65ed177e9ef
Austen <austen.ruzicka@gmail.com>
2024-07-18 17:29:36 -0700

    Update PnET to v8

--- src/lib/support_libs_download.ps1
+++ src/lib/support_libs_download.ps1
@@ -1,21 +1,18 @@
 # LANDIS-II support library GitHub URL
-$master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v7/raw/master/"
+$master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v8/raw/main/"
 
 
 #************************************************
 # LANDIS-II support library dependencies
 # Modify here when any dependencies changed 
 
-$dlls = "Landis.Library.AgeOnlyCohorts-v3.dll",
-"Landis.Library.Cohorts-v2.dll",
-"Landis.Library.BiomassCohorts-v3.dll",
+$dlls = "Landis.Library.UniversalCohorts-v1.dll",
 "Landis.Library.Metadata-v2.dll",
 "Landis.Library.Parameters-v2.dll",
-"Landis.Library.Biomass-v2.dll",
-"Landis.Library.Climate-v4.1.dll"
+"Landis.Library.Climate-v4.4.dll"
 "Landis.Library.Succession-v9.dll",
-"Landis.Library.PnETCohorts-v1.dll"
-"Landis.Library.InitialCommunity-vBiomassCohorts.dll"
+"Landis.Library.PnETCohorts-v2.dll"
+"Landis.Library.InitialCommunity-Universal.dll"
 #************************************************
 
 
