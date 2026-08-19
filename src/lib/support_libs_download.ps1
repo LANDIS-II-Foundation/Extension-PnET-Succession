40fb02f06e6976423484a15e7bc3c048445bf219
Melissa Lucash <mslucash@gmail.com>
2026-08-19 12:36:55 -0700

    download UC and PnET cohorts from support dir

--- src/lib/support_libs_download.ps1
+++ src/lib/support_libs_download.ps1
@@ -7,12 +7,12 @@ $master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v8/raw/m
 # Modify here when any dependencies changed 
 
 $dlls = 
-#"Landis.Library.UniversalCohorts-v2.dll",
+"Landis.Library.UniversalCohorts-v2.dll",
 "Landis.Library.Metadata-v2.dll",
 "Landis.Library.Parameters-v2.dll",
 "Landis.Library.Climate-v5.dll",
 "Landis.Library.Succession-v10.dll",
-#"Landis.Library.PnETCohorts-v2.dll",
+"Landis.Library.PnETCohorts-v2.dll",
 "Landis.Library.InitialCommunity.Universal-v2.dll"
 #************************************************
 
